966b01eb70b9ea1b098ed6fbd75643e9d27ec318
TsukichanOnTermius <tsukichan555.dev@gmail.com>
2025-03-26 23:12:55 +0900

    aboutページを実装

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,13 +1,31 @@
 import React from 'react';
+import { client } from '@/libs/microcms';
+import Image from 'next/image';
+import { parseHtmlContent } from '@/utils/htmlParser';
 
+//get about page data from microCMS
+async function getAboutContent() {
+    const data = await client.get({
+        endpoint: `about/`,
+    });
+    return data;
+}
 
-const AboutPage = () => {
+//generate about page
+export default async function AboutPage() {
+    const aboutContent = await getAboutContent();
+
+    //console.log(aboutContent.contents);
+
+    // Parse the HTML content to replace img tags with Next.js Image components
+    const parsedContent = aboutContent.contents[0].aboutme ? parseHtmlContent(aboutContent.contents[0].aboutme) : null;
+
     return (
-        <div style={{ padding: '20px', fontFamily: 'Arial, sans-serif' }}>
-            <h1>About Us</h1>
-
-        </div>
-    );
-};
-
-export default AboutPage;
+        <article>
+            <h1>私について</h1>
+            <section>
+                {parsedContent}
+            </section>
+        </article>
+    )
+}
